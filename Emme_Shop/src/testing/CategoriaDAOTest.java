eb0b50a9c3719d863a2c15d02b819d3eb340f639
CetrangoloMario <55892147+CetrangoloMario@users.noreply.github.com>
2020-01-27 20:28:06 +0100

    ok

---
+++ Emme_Shop/src/testing/CategoriaDAOTest.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategoriaDAOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
