150c028e9456291549bb0357d19835066c71b942
manlio <mansant98@gmail.com>
2020-01-29 22:22:17 +0100

    NegozioTest e ProdottoTest

---
+++ Emme_Shop/src/testing/NegozioTest.java
@@ -0,0 +1,177 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import managernegozio.Negozio;
+
+class NegozioTest {
+
+	@Test
+	void testNegozioCostruttore() {
+		Negozio negozio = new Negozio();
+		assertNotNull(negozio);
+	}
+	
+	@Test
+	void testGetLogo() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("images/negozi/Pacifico/Pacifico.png",negozio.getLogo());
+	}
+	
+	@Test
+	void testSetLogo() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setLogo("images/negozi/Pacifico/Pac.png");
+		assertEquals("images/negozi/Pacifico/Pac.png",negozio.getLogo());
+	}
+	
+	@Test
+	void testGetNomeNegozio() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("Pacifico",negozio.getNomeNegozio());
+	}
+	
+	@Test
+	void testSetNomeNegozio() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setNomeNegozio("Nutella");
+		assertEquals("Nutella",negozio.getNomeNegozio());
+	}
+	
+	
+	@Test
+	void testGetUsernameVenditore() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("Mario",negozio.getUsernameVenditore());
+	}
+	
+	@Test
+	void testSetUsernameVenditore() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setUsernameVenditore("Manlio");;
+		assertEquals("Manlio",negozio.getUsernameVenditore());
+	}
+	
+	@Test
+	void testGetDesign() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("left",negozio.getDesign());
+	}
+	
+	
+	@Test
+	void testSetDesign() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setDesign("right");
+		assertEquals("right",negozio.getDesign());
+	}
+	
+	@Test
+	void testGetColore() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("Verde",negozio.getColore());
+	}
+	
+	
+	@Test
+	void testSetColore() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setColore("Blue");
+		assertEquals("Blue",negozio.getColore());
+	}
+	
+	@Test
+	void testGetPartitaIva() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("11111111112",negozio.getPartitaIva());
+	}
+	
+	
+	@Test
+	void testSetPartitaIva() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setPartitaIva("123456789");;
+		assertEquals("123456789",negozio.getPartitaIva());
+	}
+	
+	@Test
+	void testGetDataIscrizione() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("2019-05-01",negozio.getDataIscrizione());
+	}
+	
+	
+	@Test
+	void testSetDataIscrizione() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setDataIscrizione("2019-05-02");
+		assertEquals("2019-05-02",negozio.getDataIscrizione());
+	}
+	
+	@Test
+	void testGetDescrizione() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("Pacifico nasce nel 1920, a Herzogenaurach",negozio.getDescrizione());
+	}
+	
+	
+	@Test
+	void testSetDescrizione() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setDescrizione("Pacifico nasce nel 1922");
+		assertEquals("Pacifico nasce nel 1922",negozio.getDescrizione());
+	}
+	
+	@Test
+	void testGetVia() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("senerchia",negozio.getVia());
+	}
+	
+	
+	@Test
+	void testSetVia() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setVia("Petrarca");
+		assertEquals("Petrarca",negozio.getVia());
+	}
+	
+	@Test
+	void testGetCitta() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("casagiove",negozio.getCitta());
+	}
+	
+	
+	@Test
+	void testSetCitta() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setCitta("caserta");
+		assertEquals("caserta",negozio.getCitta());
+	}
+	
+	
+	@Test
+	void testGetCap() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		assertEquals("81023",negozio.getCap());
+	}
+	
+	
+	@Test
+	void testSetCap() {
+		Negozio negozio = new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		negozio.setCap("81021");
+		assertEquals("81021",negozio.getCap());
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ Emme_Shop/src/testing/ProdottoTest.java
@@ -0,0 +1,152 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import managernegozio.Prodotto;
+
+class ProdottoTest {
+
+	@Test
+	void testProdottoCostruttore() {
+		Prodotto p = new Prodotto();
+		assertNotNull(p);
+	}
+	
+	@Test
+	void testGetIdProdotto() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals(11111,p.getIdProdotto());
+	}
+	
+	@Test
+	void testSetIdProdotto() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setIdProdotto(12);
+		assertEquals(12,p.getIdProdotto());
+	}
+	
+	
+	@Test
+	void testGetIva() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals(22,p.getIva());
+	}
+	
+	@Test
+	void testSetIva() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setIva(23);
+		assertEquals(23,p.getIva());
+	}
+	
+	@Test
+	void testGetPath() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals("images/negozi/Pacifico/500.jpg",p.getPath());
+	}
+	
+	@Test
+	void testSetPath() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setPath("images/negozi/Pacifico/500.jpg");
+		assertEquals("images/negozi/Pacifico/500.jpg",p.getPath());
+	}
+	
+	
+	@Test
+	void testGetPrezzo() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals(10.5f,p.getPrezzo());
+	}
+	
+	@Test
+	void testSetPrezzo() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setPrezzo(11.5f);
+		assertEquals(11.5f,p.getPrezzo());
+	}
+	
+	@Test
+	void testGetQuantita() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals(4,p.getQuantita());
+	}
+	
+	@Test
+	void testSetQuantita() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setQuantita(5);
+		assertEquals(5,p.getQuantita());
+	}
+	
+	@Test
+	void testGetSconto() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals(23,p.getSconto());
+	}
+	
+	@Test
+	void testSetSconto() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setSconto(25);
+		assertEquals(25,p.getSconto());
+	}
+	
+	
+	@Test
+	void testGetDescrizione() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals("Molto buoni e gustosi",p.getDescrizione());
+	}
+	
+	@Test
+	void testSetDescrizione() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setDescrizione("Ne buoni e ne gustosi");
+		assertEquals("Ne buoni e ne gustosi",p.getDescrizione());
+	}
+	
+	
+	@Test
+	void testGetNomeNegozio() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals("Pacifico",p.getNomeNegozio());
+	}
+	
+	@Test
+	void testSetNomeNegozio() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setNomeNegozio("Pac");
+		assertEquals("Pac",p.getNomeNegozio());
+	}
+	
+	@Test
+	void testGetNomeCategoria() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals("Biscotti",p.getNomeCategoria());
+	}
+	
+	@Test
+	void testSetNomeCategoria() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setNomeCategoria("Libri");
+		assertEquals("Libri",p.getNomeCategoria());
+	}
+	
+	@Test
+	void testGetNome() {
+		 Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		 assertEquals("ringo",p.getNome());
+	}
+	
+	@Test
+	void testSetNome() {
+		Prodotto p=new Prodotto(11111,"Pacifico","Biscotti","ringo",22,"images/negozi/Pacifico/500.jpg",10.5f,4,23,"Molto buoni e gustosi");
+		p.setNome("briosche");
+		assertEquals("briosche",p.getNome());
+	}
+	
+
+}
