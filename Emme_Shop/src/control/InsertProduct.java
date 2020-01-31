3f63b4a277619b84e5dd29cc47d2fe3071e10ffb
manlio <mansant98@gmail.com>
2020-01-31 23:54:52 +0100

    tc5 inserisci prodotto completato

--- Emme_Shop/src/control/InsertProduct.java
+++ Emme_Shop/src/control/InsertProduct.java
@@ -1,6 +1,8 @@
 package control;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,11 +32,35 @@ public class InsertProduct extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String risposta = "";
 		String address="";
+		PrintWriter writer = response.getWriter();
+		String prezzo = "";
 		try {
 			HttpSession session=request.getSession();
 			session.setAttribute("urlLogoProdotto", "images/favicon.ico");
+			//serve per fare il testing
+			prezzo = request.getParameter("prezzoProdotto");
+			if(prezzo.equals("")) {
+				prezzo = "0";
+			}
+			
+			String iva = request.getParameter("ivaProdotto");
+			if(iva.equals("")) {
+				iva = "111";
+			}
+			
+			String qta = request.getParameter("qtaProdotto");
+			
+			if(qta.equals("")) {
+				qta = "000";
+			}
+			
+			String sconto = request.getParameter("scontoProdotto");
+			if(sconto.equals("")) {
+				sconto = "000";
+			}
+			
             
 			Prodotto prodotto=new Prodotto();
 
@@ -44,34 +70,78 @@ public class InsertProduct extends HttpServlet {
 			prodotto.setNomeCategoria(request.getParameter("nomeCategoria"));
 			prodotto.setNome(request.getParameter("nomeProdotto"));
 			prodotto.setDescrizione(request.getParameter("descProdotto"));
-			prodotto.setIva(Integer.parseInt(request.getParameter("ivaProdotto")));
-			prodotto.setPrezzo(Float.parseFloat(request.getParameter("prezzoProdotto")));
-			prodotto.setQuantita(Integer.parseInt(request.getParameter("qtaProdotto")));
-			prodotto.setSconto(Integer.parseInt(request.getParameter("scontoProdotto")));
+			prodotto.setIva(Integer.parseInt(iva));
+			prodotto.setPrezzo(Float.parseFloat(prezzo));
+			prodotto.setQuantita(Integer.parseInt(qta));
+			prodotto.setSconto(Integer.parseInt(sconto));
 			prodotto.setPath("images/favicon.ico");
 			
-
 			
-			if(!prodotto.getNomeNegozio().equals("Some problem")) {
-				prodotto=modelprod.addProdotto(prodotto);
-				session.setAttribute("nomeProdottoImage",prodotto.getIdProdotto());
-				session.setAttribute("nomeProdottoCat",prodotto.getNomeCategoria());
-				
-				address="venditore/uploadImageProdotto.jsp";
+			if(Controlli.isStreet(prodotto.getNome())) {
+				System.out.println("ok nome prodotto");
+				if(Controlli.isDesc(prodotto.getDescrizione())) {
+					System.out.println("ok descrizione");
+					if(Controlli.isSconto(iva)) {
+						System.out.println("ok iva");
+						if(Controlli.isPrezzo(prezzo)) {
+							System.out.println("ok prezzo");
+							if(Controlli.isSconto(qta)) {
+								System.out.println("ok quantita");
+								if(Controlli.isSconto(sconto)) {
+									risposta = "ok";
+									System.out.println("ok sconto");
+									if(!prodotto.getNomeNegozio().equals("Some problem")) {
+										prodotto=modelprod.addProdotto(prodotto);
+										session.setAttribute("nomeProdottoImage",prodotto.getIdProdotto());
+										session.setAttribute("nomeProdottoCat",prodotto.getNomeCategoria());
+										
+										address="venditore/uploadImageProdotto.jsp";
+										response.sendRedirect(address);
+									}
+								}else {
+									System.out.println("sconto formato errato");
+									risposta = "sconto non rispetta formato";
+									address="venditore/inserisci-prodotto.jsp";
+									response.sendRedirect(address);
+								}
+							}else {
+								System.out.println("quantità formato errato");
+								risposta = "quantita non rispetta formato";
+								address="venditore/inserisci-prodotto.jsp";
+								response.sendRedirect(address);
+							}
+						}else {
+							System.out.println("prezzo formato errato");
+							risposta = "prezzo non rispetta formato";
+							address="venditore/inserisci-prodotto.jsp";
+							response.sendRedirect(address);
+						}
+					}else {
+						System.out.println("iva formato errato");
+						risposta = "iva non rispetta formato";
+						address="venditore/inserisci-prodotto.jsp";
+						response.sendRedirect(address);
+					}
+				}else {
+					System.out.println("descrizione formato errato");
+					risposta = "descrizione non rispetta formato";
+					address="venditore/inserisci-prodotto.jsp";
+					response.sendRedirect(address);
+				}
+			}else {
+				System.out.println("nome prodotto formato errato");
+				risposta = "nome prodotto non rispetta formato";
+				address="venditore/inserisci-prodotto.jsp";
 				response.sendRedirect(address);
 			}
-			else {
-				response.sendRedirect("errore.jsp");
-			}
-		}
+			writer.print(risposta);
+		}	
 		catch(Exception e) {
 			 address="errore.jsp";
 			 response.sendRedirect(address);
 			System.out.print(e);
 		}
-		finally {
-			
-		}
+		
 	}
 
 	/**
---
+++ Emme_Shop/src/testing/tc5_0_InserisciProdotto.java
@@ -0,0 +1,417 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import control.InsertCategoria;
+import control.InsertProduct;
+import manageraccouting.Venditore;
+import managernegozio.Categoria;
+import managernegozio.Negozio;
+import model.CategoriaDAO;
+import model.NegozioDAO;
+import model.VenditoreDAO;
+
+public class tc5_0_InserisciProdotto {
+	InsertProduct myServlet;
+	
+	Venditore vend;
+	VenditoreDAO modelv=new VenditoreDAO();
+	Negozio neg;
+	NegozioDAO modeln=new NegozioDAO();
+	CategoriaDAO modelc=new CategoriaDAO();
+	Categoria cat;
+	
+	@Mock
+	HttpServletRequest request;
+	
+	@Mock
+	HttpServletResponse response;
+	
+	@Mock
+	HttpSession session;
+	
+	@BeforeEach
+	public void setUp() throws SQLException{
+		request = mock(HttpServletRequest.class);
+		response = mock(HttpServletResponse.class);
+		session = mock(HttpSession.class);
+		myServlet = new InsertProduct();
+		
+		vend= new Venditore("Mario","Password0#","Mario","Cetrangolo","cetrangolomario98@gmail.com","F","3415578614","Umberto1","Salerno","85100");
+		modelv.addVenditore(vend);
+		neg=new Negozio("Pacifico","Mario","left","Verde","11111111112","2019-05-01","Pacifico nasce nel 1920, a Herzogenaurach","senerchia","casagiove","81023","images/negozi/Pacifico/Pacifico.png");
+		modeln.addNegozio(neg);
+		cat=new Categoria( "Pacifico",  "Biscotti", "path/biscotti",  "descrizione");
+		modelc.addCategoria(cat);
+	}
+	
+	@Test
+	public void tc_5_0_1() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("nome prodotto non rispetta formato", output.toString());
+	}
+	
+	
+	
+	@Test
+	public void tc_5_0_2() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa 100% Originale");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("nome prodotto non rispetta formato", output.toString());
+	}
+	
+	@Test
+	public void tc_5_0_3() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("descrizione non rispetta formato", output.toString());
+	}
+	
+	@Test
+	public void tc_5_0_4() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("#Felpa Nike");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("descrizione non rispetta formato", output.toString());
+	}
+	
+	
+	@Test
+	public void tc_5_0_5() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("prezzo non rispetta formato", output.toString());
+	}
+	
+	
+	@Test
+	public void tc_5_0_6() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("prezzo non rispetta formato", output.toString());
+	}
+	
+	
+	
+	@Test
+	public void tc_5_0_7() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("iva non rispetta formato", output.toString());
+	}
+	
+	
+	@Test
+	public void tc_5_0_8() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("223");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("100");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("iva non rispetta formato", output.toString());
+	}
+	
+	@Test
+	public void tc_5_0_9() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("quantita non rispetta formato", output.toString());
+	}
+	
+	
+	@Test
+	public void tc_5_0_10() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("000");
+		when(request.getParameter("scontoProdotto")).thenReturn("20");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("quantita non rispetta formato", output.toString());
+	}
+	
+	@Test
+	public void tc_5_0_11() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("10");
+		when(request.getParameter("scontoProdotto")).thenReturn("200");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("sconto non rispetta formato", output.toString());
+	}
+	
+	@Test
+	public void tc_5_0_12() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("10");
+		when(request.getParameter("scontoProdotto")).thenReturn("");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("sconto non rispetta formato", output.toString());
+	}
+	
+	
+	@Test
+	public void tc_5_0_13() throws IOException, ServletException {
+		StringWriter output=new StringWriter();
+		PrintWriter out=new PrintWriter(output);
+		
+		when(request.getSession()).thenReturn(session);
+		when(session.getAttribute("urlLogoProdotto")).thenReturn("images/favicon.ico");
+		
+		when(request.getParameter("nomeNegozio")).thenReturn(neg.getNomeNegozio());
+		when(request.getParameter("nomeCategoria")).thenReturn(cat.getNomeCategoria());
+		when(request.getParameter("nomeProdotto")).thenReturn("Felpa");
+		when(request.getParameter("descProdotto")).thenReturn("Felpa Nike sportiva");
+		when(request.getParameter("ivaProdotto")).thenReturn("22");
+		when(request.getParameter("prezzoProdotto")).thenReturn("24.99");
+		when(request.getParameter("qtaProdotto")).thenReturn("10");
+		when(request.getParameter("scontoProdotto")).thenReturn("23");
+		
+		
+		when(response.getWriter()).thenReturn(out);
+		
+		myServlet.doPost(request, response);
+		System.out.println(output.toString());
+		assertEquals("ok", output.toString());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@AfterEach
+	public void tearDown() throws SQLException {
+		modelv.deleteVenditore("Mario");
+		modeln.deleteShop("Pacifico");
+		modelc.deleteCategory("Pacifico", "Biscotti");
+	}
+	
+	
+	
+
+}
